7116d19d5d739a0f4d5dc3ef2e8009b2543ad97b
augustodantas <laurentino.dantas@usp.br>
2019-10-24 17:13:17 -0300

    Deploy no Heroku

--- recommendation_system/settings.py
+++ recommendation_system/settings.py
@@ -98,19 +98,8 @@ WSGI_APPLICATION = 'recommendation_system.wsgi.application'
 #     }
 # }
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {     
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'active-aging',
@@ -119,7 +108,7 @@ else:
             'HOST': 'localhost',
             'PORT': '5432',
         }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -188,3 +177,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'RlJXpzctTjd9BncZJrdK7ShZ'
 
 LOGIN_REDIRECT_URL = '/teste/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
