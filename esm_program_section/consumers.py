7d6051d2fd55fb23e2dc37cba1e97eb0fa8bcc34
augustodantas <laurentino.dantas@usp.br>
2021-04-19 21:20:35 -0300

    decimo quarto  deploy socket

--- esm_program_section/consumers.py
+++ esm_program_section/consumers.py
@@ -7,8 +7,7 @@ import json
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
-        #self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_name = "fase";
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
--- esm_program_section/routing.py
+++ esm_program_section/routing.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import consumers
 
 
 websocket_urlpatterns = [
-    url(r'ws/chat/', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
